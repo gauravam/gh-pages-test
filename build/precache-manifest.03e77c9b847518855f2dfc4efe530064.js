f2e85c0b6664fa9920dea28695482b0c41a77d66
Diego Inácio <diegodci@gmail.com>
2020-05-11 16:10:31 -0300

    Upload build

---
+++ build/precache-manifest.03e77c9b847518855f2dfc4efe530064.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17b31e04e7c5dce7afa7a66fee07435",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "2d8245c0ba520a58516e",
+    "url": "/gh-pages-test/static/css/main.e8920902.chunk.css"
+  },
+  {
+    "revision": "f993b2317997b47a00af",
+    "url": "/gh-pages-test/static/js/2.d700a3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-test/static/js/2.d700a3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d8245c0ba520a58516e",
+    "url": "/gh-pages-test/static/js/main.3a1f6dc3.chunk.js"
+  },
+  {
+    "revision": "8ca1805972bc3cf0ef01",
+    "url": "/gh-pages-test/static/js/runtime-main.bf720e5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
